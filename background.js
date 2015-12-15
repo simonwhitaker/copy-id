0c15e06f245be8f707b4f27ef61bf75aaf0a0ad6
Simon Whitaker <swhitaker@fb.com>
2015-12-15 12:22:04 +0000

    Consider fragments and query param values when looking for IDs

--- background.js
+++ background.js
@@ -38,14 +38,44 @@ function getComponentScore(component) {
 
 chrome.browserAction.onClicked.addListener(function(tab) {
   var url = tab.url;
+  var components = []; // List of URL components that might be an ID
 
-  var path = tab.url.replace(/^.+:\/\/.+?\//, '');
-  var components = path.split('/');
+  // Remove everything up to and including the end of the domain. e.g. given
+  // http://foo.com/bar/wibble?baz=123#456, this regex renders
+  // "bar/wibble?baz=123#456"
+  var url_minus_domain = tab.url.replace(/^.+:\/\/.+?\//, '');
+
+  // Look for a fragment, consider it as a possible ID
+  var remainder_and_fragment = url_minus_domain.split('#');
+  if (remainder_and_fragment.length > 1) {
+    // Add the fragment to the list of possible IDs
+    components.push(remainder_and_fragment[1]);
+  }
+
+  // Look for a query string, consider its values as possible IDs
+  var path_and_query = remainder_and_fragment[0].split('?');
+  if (path_and_query.length > 1) {
+    var query_pairs = path_and_query[1].split('&');
+    query_pairs.forEach(function(query_pair) {
+      var key_value = query_pair.split('=');
+      if (key_value.length > 1) {
+        components.push(key_value[1]);
+      }
+    });
+  }
+
+  var path_components = path_and_query[0].split('/');
+  path_components.forEach(function(path_component) {
+    if (path_component.length > 0) {
+      components.push(path_component);
+    }
+  });
+
   var bestScore = 0;
   var mostLikelyIDComponent = null;
   components.forEach(function(component) {
     var score = getComponentScore(component);
-    // console.log('Component: "' + component + '", score: ' + score);
+    console.log('Component: "' + component + '", score: ' + score);
     if (score > bestScore) {
       mostLikelyIDComponent = component;
       bestScore = score;
